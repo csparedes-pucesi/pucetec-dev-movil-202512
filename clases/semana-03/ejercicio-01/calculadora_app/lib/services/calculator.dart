e7092d19f2ef1e246cc18a946b51160fc11b56bb
“David <datobarmi@pucesi.edu.ec>
2025-05-14 23:55:01 -0500

    calculadora

--- clases/semana-03/ejercicio-01/calculadora_app/lib/services/calculator.dart
+++ clases/semana-03/ejercicio-01/calculadora_app/lib/services/calculator.dart
@@ -1,26 +1,77 @@
+class CalculatorService {
+  String _number = '0';
+  String _previousNumber = '0';
+  String _operation = '';
 
+  String get number => _number;
 
-// formula
+  void clear() {
+    _number = '0';
+    _previousNumber = '0';
+    _operation = '';
+  }
 
-// numero
+  void deleteLastDigit() {
+    if (_number.length == 1 || (_number.length == 2 && _number.startsWith('-'))) {
+      _number = '0';
+    } else {
+      _number = _number.substring(0, _number.length - 1);
+    }
+  }
 
-// numero previo
+  void toggleSign() {
+    if (_number.startsWith('-')) {
+      _number = _number.substring(1);
+    } else if (_number != '0') {
+      _number = '-$_number';
+    }
+  }
 
-// ultima operación
+  void appendNumber(String value) {
+    if (_number == '0' && value != '.') {
+      _number = value;
+    } else if (value == '.' && _number.contains('.')) {
+      return;
+    } else {
+      _number += value;
+    }
+  }
 
-// calcular el resultado
+  void setOperation(String operation) {
+    _previousNumber = _number;
+    _number = '0';
+    _operation = operation;
+  }
 
-// calcular el subresultado
+  void calculateResult() {
+    final double num1 = double.tryParse(_previousNumber) ?? 0;
+    final double num2 = double.tryParse(_number) ?? 0;
+    double result = 0;
 
-// limpiar la calculadora
+    switch (_operation) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        result = num2 == 0 ? 0 : num1 / num2;
+        break;
+      case '%':
+        result = num1 % num2;
+        break;
+      default:
+        return;
+    }
 
-// borrar el ultimo numero
-
-// cambiar toggle signo
-
-// construir numero ingresado
-
-// definir ultimo numero
-
-// definir operaciones
-
+    _number = result.toString().endsWith('.0')
+        ? result.toInt().toString()
+        : result.toString();
+    _previousNumber = '0';
+    _operation = '';
+  }
+}
